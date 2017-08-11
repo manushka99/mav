5aa7f9abdf81fbf605a90512a3822016cbd0fa34
Палехов В.А <PalehovVA@alfastrah.ru>
2017-08-11 15:20:37 +0700

    test

--- src/main/java/AppletMouseListener.java
+++ src/main/java/AppletMouseListener.java
@@ -1,24 +1,24 @@
-import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import static java.awt.Color.BLUE;
 
 public class AppletMouseListener implements MouseListener {
-    public Color colorRect = BLUE;
     public int X;
     public int Y;
     public int X2;
     public int Y2;
-
+    public int P;
     public void mouseClicked(MouseEvent me) {
-        // Сохранить предыдущие координаты точки
-        X2 = X;
-        Y2 = Y;
-        // получить новые координаты точки
-        int x = me.getX();
-        int y = me.getY();
+        if (me.getButton() == MouseEvent.BUTTON1) {
+            // Сохранить предыдущие координаты точки
+            X2 = X;
+            Y2 = Y;
+            // получить новые координаты точки
+            int x = me.getX();
+            int y = me.getY();
             X = x;
             Y = y;
+            P = 25;
+        }
     }
 
     public void mouseEntered(MouseEvent e) {}
--- src/main/java/Canvas.java
+++ src/main/java/Canvas.java
@@ -1,17 +1,21 @@
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
 
 public class Canvas extends JApplet {
 AppletMouseListener appletMouseListener = new AppletMouseListener();
 
     public void init() {
-        setBackground(Color.WHITE);
         addMouseListener(appletMouseListener);
-   }
+    }
 
-   public void paint(Graphics g) {
-        g.drawLine(appletMouseListener.X,appletMouseListener.Y,appletMouseListener.X2, appletMouseListener.Y2);
-        g.setColor(appletMouseListener.colorRect);
+
+    public void paint(Graphics g) {
+
+        g.drawLine(appletMouseListener.X - appletMouseListener.P, appletMouseListener.Y - appletMouseListener.P, appletMouseListener.X - appletMouseListener.P, appletMouseListener.Y + appletMouseListener.P);
+        g.drawLine(appletMouseListener.X + appletMouseListener.P, appletMouseListener.Y - appletMouseListener.P, appletMouseListener.X + appletMouseListener.P, appletMouseListener.Y + appletMouseListener.P);
+        g.drawLine(appletMouseListener.X - appletMouseListener.P, appletMouseListener.Y - appletMouseListener.P, appletMouseListener.X + appletMouseListener.P, appletMouseListener.Y - appletMouseListener.P);
+        g.drawLine(appletMouseListener.X - appletMouseListener.P, appletMouseListener.Y + appletMouseListener.P, appletMouseListener.X + appletMouseListener.P, appletMouseListener.Y + appletMouseListener.P);
+
         repaint();
     }
 
