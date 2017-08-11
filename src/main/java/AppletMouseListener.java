ba0e3c3903639a898b824e9f7951b6365c172f96
Палехов В.А <PalehovVA@alfastrah.ru>
2017-08-11 16:35:23 +0700

    + Polygon

--- src/main/java/AppletMouseListener.java
+++ src/main/java/AppletMouseListener.java
@@ -1,12 +1,16 @@
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sqrt;
+
 public class AppletMouseListener implements MouseListener {
     public int X;
     public int Y;
     public int X2;
     public int Y2;
     public int P;
+    public int Q = 1;
     public void mouseClicked(MouseEvent me) {
         if (me.getButton() == MouseEvent.BUTTON1) {
             // Сохранить предыдущие координаты точки
@@ -18,6 +22,14 @@ public class AppletMouseListener implements MouseListener {
             X = x;
             Y = y;
             P = 25;
+            int rad = (int) (sqrt(2.) * P);
+            double p = cos(3.14 / 4);
+            double pr = rad * p;
+            int q = (int) pr;
+
+            Q = 50;
+            System.out.println(" P : " + q + "  rad: " + Q + "  pr:" + pr);
+
         }
     }
 
--- src/main/java/Canvas.java
+++ src/main/java/Canvas.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class Canvas extends JApplet {
 AppletMouseListener appletMouseListener = new AppletMouseListener();
 
+
     public void init() {
         addMouseListener(appletMouseListener);
     }
@@ -11,11 +12,15 @@ AppletMouseListener appletMouseListener = new AppletMouseListener();
 
     public void paint(Graphics g) {
 
-        g.drawLine(appletMouseListener.X - appletMouseListener.P, appletMouseListener.Y - appletMouseListener.P, appletMouseListener.X - appletMouseListener.P, appletMouseListener.Y + appletMouseListener.P);
+       /* g.drawLine(appletMouseListener.X - appletMouseListener.P, appletMouseListener.Y - appletMouseListener.P, appletMouseListener.X - appletMouseListener.P, appletMouseListener.Y + appletMouseListener.P);
         g.drawLine(appletMouseListener.X + appletMouseListener.P, appletMouseListener.Y - appletMouseListener.P, appletMouseListener.X + appletMouseListener.P, appletMouseListener.Y + appletMouseListener.P);
         g.drawLine(appletMouseListener.X - appletMouseListener.P, appletMouseListener.Y - appletMouseListener.P, appletMouseListener.X + appletMouseListener.P, appletMouseListener.Y - appletMouseListener.P);
         g.drawLine(appletMouseListener.X - appletMouseListener.P, appletMouseListener.Y + appletMouseListener.P, appletMouseListener.X + appletMouseListener.P, appletMouseListener.Y + appletMouseListener.P);
-
+        */
+        g.drawPolygon(
+                new Polygon(new int[]{100 + appletMouseListener.Q, 100 + appletMouseListener.Q, 200 + appletMouseListener.Q, 200 + appletMouseListener.Q},
+                        new int[]{100, 200 + appletMouseListener.Q, 200 - appletMouseListener.Q, 100 - appletMouseListener.Q}, 4)
+        );
         repaint();
     }
 
